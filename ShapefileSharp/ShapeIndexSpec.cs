8cbfb183f3fef41f2e608be6a186ea23b56e28c5
Matt Gerginski <mattgerg@gmail.com>
2016-05-11 00:42:29 -0400

    * ShapeIndexSpec -- Position is zero-based... doh!

--- ShapefileSharp/ShapeIndexSpec.cs
+++ ShapefileSharp/ShapeIndexSpec.cs
@@ -5,7 +5,7 @@
         public const uint HeaderPos = 0;
         public const uint HeaderBytes = 100;
 
-        public const uint FirstRecordPos = HeaderBytes + 1;
+        public const uint FirstRecordPos = HeaderBytes;
         public const uint RecordBytes = 8;
 
         public static uint GetRecordPos(uint recordIndex)
