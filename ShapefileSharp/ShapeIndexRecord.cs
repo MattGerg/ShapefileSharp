468ad14571d0738f396af6b81283589eca98c8cb
Matt Gerginski <mattgerg@gmail.com>
2016-05-10 23:40:41 -0400

    * ShapeIndexRecord -- Override Equals()

--- ShapefileSharp/ShapeIndexRecord.cs
+++ ShapefileSharp/ShapeIndexRecord.cs
@@ -4,5 +4,37 @@
     {
         public WordCount Offset { get; set; }
         public WordCount ContentLength { get; set; }
+
+        public override int GetHashCode()
+        {
+            int hash = 13;
+
+            hash = (hash * 7) + Offset.GetHashCode();
+            hash = (hash * 7) + ContentLength.GetHashCode();
+
+            return hash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as ShapeIndexRecord;
+
+            if (other == null)
+            {
+                return false;
+            }
+
+            if (!Offset.Equals(other.Offset))
+            {
+                return false;
+            }
+
+            if (!ContentLength.Equals(other.ContentLength))
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
