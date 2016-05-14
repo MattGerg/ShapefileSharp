58b21679b467716df90bd44c6390aa84b6fa3f07
Matt Gerginski <mattgerg@gmail.com>
2016-05-14 14:00:02 -0400

    * ShxSpec -- Add Pos and Length for fields

--- ShapefileSharp/Spec/ShxSpec.cs
+++ ShapefileSharp/Spec/ShxSpec.cs
@@ -15,7 +15,19 @@
                 public static WordCount Pos { get; } = Header.Length;
             }
 
-            public static WordCount Length { get; } = new WordCount(2);
+            private static class Offset
+            {
+                public static WordCount Pos { get; } = new WordCount(0);
+                public static WordCount Length { get; } = new WordCount(1);
+            }
+
+            private static class ContentLength
+            {
+                public static WordCount Pos { get; } = new WordCount(1);
+                public static WordCount Length { get; } = new WordCount(1);
+            }
+
+            public static WordCount Length { get; } = Offset.Length + ContentLength.Length;
 
             public static WordCount GetPos(int recordIndex)
             {
