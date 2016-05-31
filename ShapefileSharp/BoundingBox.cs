7a109a62f93f196a606cb0b94b33a2b2c405d34b
Matt Gerginski <mattgerg@gmail.com>
2016-05-30 22:11:33 -0400

    * BoundingBox -- Overload constructor

--- ShapefileSharp/BoundingBox.cs
+++ ShapefileSharp/BoundingBox.cs
@@ -1,11 +1,16 @@
 ﻿namespace ShapefileSharp
 {
+    //TODO: Mutable struct... bad practice?
     public struct BoundingBox<T> : IBoundingBox<T> where T:IPoint
     { 
-        public BoundingBox(IBoundingBox<T> box)
+        public BoundingBox(IBoundingBox<T> box) : this(box.Min, box.Max)
         {
-            Min = box.Min;
-            Max = box.Max;
+        }
+
+        public BoundingBox(T min, T max)
+        {
+            Min = min;
+            Max = max;
         }
 
         public T Min { get; set; }
