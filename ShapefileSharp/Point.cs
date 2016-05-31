ab347d7ef3de417655e570d0910a5e0349adaf56
Matt Gerginski <mattgerg@gmail.com>
2016-05-30 22:11:03 -0400

    * Point -- Add Empty field

--- ShapefileSharp/Point.cs
+++ ShapefileSharp/Point.cs
@@ -2,8 +2,11 @@
 {
     //TODO: Shouldn't an IPointZ should equal an IPoint based only on X+Y?  
     //      With this struct, it won't...
+    //TODO: Mutable struct... bad practice?
     public struct Point : IPoint, IPointM, IPointZ
     {
+        public static readonly Point Empty = new Point();
+
         public double X { get; set; }
         public double Y { get; set; }
         public double Z { get; set; }
