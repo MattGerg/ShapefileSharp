91e18879c81c6fd2b0c57fe9f4f38d69bb326b3f
Matt Gerginski <mattgerg@gmail.com>
2016-06-19 21:58:29 -0400

    * PartType -- Added enumeration in prep for MultiPatch

---
+++ ShapefileSharp/PartType.cs
@@ -0,0 +1,12 @@
+﻿namespace ShapefileSharp
+{
+    public enum PartType
+    {
+        TriangleStrip = 0,
+        TriangleFan = 1,
+        OuterRing = 2,
+        InnerRing = 3,
+        FirstRing = 4,
+        Ring
+    }
+}
